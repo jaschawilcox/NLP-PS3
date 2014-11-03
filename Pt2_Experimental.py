6616ea988360a9015c321b0b2299fae1e333fbcb
voronoi <adityakd@gmail.com>
2014-11-03 10:44:25 -0500

    Commented Out Unsupervised
    Need to get unsupervised working

--- Pt2_Experimental.py
+++ Pt2_Experimental.py
@@ -109,39 +109,39 @@ from sklearn.linear_model import LogisticRegression
 maxent_classifier = LogisticRegression().fit(X_train, y_train)
 y_maxent_predicted = maxent_classifier.predict(X_test)
 
-# Unsupervised Topic Model Based Ckustering
-
-documents = X_train
-stoplist = stopwords.words('english')
-texts = [[word for word in document.lower().split() if word not in stoplist]
- for document in documents]
-
-dictionary = corpora.Dictionary(texts)
-corpus = [dictionary.doc2bow(text) for text in texts]
-
-tfidf = models.TfidfModel(corpus) 
-corpus_tfidf = tfidf[corpus]
-
-#lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)
-#lsi.print_topics(20)
-
-n_topics = 2
-lda = models.LdaModel(corpus_tfidf, id2word=dictionary, num_topics=n_topics)
-
-for i in range(0, n_topics):
- temp = lda.show_topic(i, 10)
- terms = []
- for term in temp:
-     terms.append(term[1])
- print "Top 10 terms for topic #" + str(i) + ": "+ ", ".join(terms)
- 
-print 
-print 'Which LDA topic maximally describes a document?\n'
-print 'Original document: ' + documents[1]
-print 'Preprocessed document: ' + str(texts[1])
-print 'Matrix Market format: ' + str(corpus[1])
-print 'Topic probability mixture: ' + str(lda[corpus[1]])
-print 'Maximally probable topic: topic #' + str(max(lda[corpus[1]],key=itemgetter(1))[0])
+## Unsupervised Topic Model Based Ckustering
+#
+#documents = X_train
+#stoplist = stopwords.words('english')
+#texts = [[word for word in document.lower().split() if word not in stoplist]
+# for document in documents]
+#
+#dictionary = corpora.Dictionary(texts)
+#corpus = [dictionary.doc2bow(text) for text in texts]
+#
+#tfidf = models.TfidfModel(corpus) 
+#corpus_tfidf = tfidf[corpus]
+#
+##lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)
+##lsi.print_topics(20)
+#
+#n_topics = 2
+#lda = models.LdaModel(corpus_tfidf, id2word=dictionary, num_topics=n_topics)
+#
+#for i in range(0, n_topics):
+# temp = lda.show_topic(i, 10)
+# terms = []
+# for term in temp:
+#     terms.append(term[1])
+# print "Top 10 terms for topic #" + str(i) + ": "+ ", ".join(terms)
+# 
+#print 
+#print 'Which LDA topic maximally describes a document?\n'
+#print 'Original document: ' + documents[1]
+#print 'Preprocessed document: ' + str(texts[1])
+#print 'Matrix Market format: ' + str(corpus[1])
+#print 'Topic probability mixture: ' + str(lda[corpus[1]])
+#print 'Maximally probable topic: topic #' + str(max(lda[corpus[1]],key=itemgetter(1))[0])
 
     
     
